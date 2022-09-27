5bfcd84432f10121e96117328081c79cc881ff6b
Marcynha01 <94566644+Marcynha01@users.noreply.github.com>
2022-09-27 16:35:03 -0300

    Add files via upload

---
+++ src/components/Header/header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaBars, FaUserAlt, FaCartPlus } from 'react-icons/fa'
+import '../Header/header.css'
+import Logo from '../Header/logo2.png'
+
+const Header = () => {
+   
+
+    return (
+
+        
+        <div className='navbar2'>
+            <div className='esquerda'>
+                <FaBars className='menu'/>
+                
+                
+                    </div>
+                    <div className='logo'>
+                    <img src={Logo} className='h-logo'/>
+                </div>
+                <div className='direita'>
+                < FaUserAlt className='user' />
+                < FaCartPlus className='carrinho' />
+                </div>
+                </div>
+                
+                
+              
+            
+                
+        
+    )
+}    
+
+export default Header
---
+++ src/components/Principal/index.js
@@ -0,0 +1,24 @@
+import { Carousel } from 'bootstrap'
+import React from 'react'
+import Header from '../Header/header'
+import Footer from '../Footer/footer'
+import Carrossel from '../Carousel/carousel'
+//import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+
+
+    return (
+
+     <> 
+    <Header/>
+    <Carrossel/>
+   
+   <Footer />
+   
+    </>
+        
+    )
+}    
+
+export default Home
